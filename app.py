fc29a773171718f800dbc06d0b42fbc9cd534a23
Matheus <139089558+Matheus77md@users.noreply.github.com>
2024-11-17 13:55:50 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 db = mysql.connector.connect(
     host="127.0.0.1",
     user="root",
-    password="senha123", 
+    password="senha123",
     database="sistema_consultas"
 )
 
